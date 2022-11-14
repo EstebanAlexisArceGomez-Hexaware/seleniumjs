a4f035746502572a406aa83431f3e09f2022415a
Esteban alexis Arce gomez <2000081765@hexaware.com>
2022-11-14 17:01:42 -0600

    * Add automation steps to go to cheapest returning selection. There is one modal that I'm not able to click

--- test/volaris.js
+++ test/volaris.js
@@ -12,7 +12,10 @@ let DT_DESTINATION = [];
 
 const getRandomLocation = async (locationArray, driver) => {
     let tempArr = await getElementsBy("css", ".destination-item", driver);
-    tempArr = await Promise.all(tempArr.map(async (airport) => await airport.getText()));
+    tempArr = await Promise.all(tempArr.map(async (airport) => {
+        const airportText = await airport.getText();
+        return airportText.includes(" New") ? airportText.replace(" New", "") : airportText
+    }));
     tempArr.forEach(element => {
         locationArray.push(element);
     });
@@ -85,6 +88,26 @@ const selectCheapestDeparture = async (driver) => {
         .then(el => el.click())
 }
 
+const selectCheapestReturning = async (driver) => {
+    await getElementBy("css", ".flightItemDetails .flightFares .fareRegular", driver)
+        .then(el => el.click());
+    const cheapestCard = await getElementBy("css", ".fareTypes .col-md-4", driver);
+    const actions = driver.actions({ async: true });
+    await actions.move({ origin: cheapestCard }).perform();
+    await cheapestCard
+        .findElement(By.css("mat-card button"))
+        .then(el => el.click())
+}
+
+const checkInfoMatches = async (departure, destination, passengers) => {
+    const departureSection = await getElementBy("css", "mbs-selected-flight .flight-info .regular .row .flightroute");
+    const flightRoute = await departureSection.getText();
+    assert(flightRoute.includes(departure) && flightRoute.includes(destination));
+    const passengerSection = await getElementBy("css", "mbs-passengers-card .mat-expansion-panel-content");
+    const selectedPassengers = await passengerSection.getText();
+    assert(selectedPassengers.includes(passengers));
+}
+
 async function exercise() {
     const driver = await new Builder().forBrowser("chrome").build();
     await driver.manage().window().maximize();
@@ -94,17 +117,57 @@ async function exercise() {
     try {
         await setDeparture(driver);
         await setDestination(driver);
-        // await setDepartureDate(driver);
-        // await setReturningDate(driver);
-        // await setPassengers(driver);
-        // await getElementBy("css", ".btn-large", driver)
-        //     .then(el => el.click());
-        // await driver.wait(until.elementIsVisible(await getElementBy("id", "tua-message", driver)), 10000);
-        // let bookingDetails = await getElementBy("css", ".route-info-heading", driver);
-        // bookingDetails = await bookingDetails.getText();
-        // assert(bookingDetails.includes(`${departure}`) && bookingDetails.includes(`${destination}`));
-        // await selectCheapestDeparture(driver);
-        // await driver.sleep(5000);
+        await setDepartureDate(driver);
+        await setReturningDate(driver);
+        await setPassengers(driver);
+        await getElementBy("css", ".btn-large", driver)
+            .then(el => el.click());
+        await driver.wait(until.elementIsVisible(await getElementBy("id", "tua-message", driver)), 15000);
+        let bookingDetails = await getElementBy("css", ".route-info-heading", driver);
+        bookingDetails = await bookingDetails.getText();
+        await selectCheapestDeparture(driver);
+        // await driver.wait(until.elementIsNotVisible(await getElementBy("id", "system-loader", driver)), 15000);
+        await driver.sleep(7000);
+        await selectCheapestReturning(driver);
+        await driver
+            .actions()
+            .move({origin: await getElementBy("css", "mat-dialog-container", driver)})
+            .perform()
+        await getElementBy("css", ".baggage-modal button", driver)
+            .then(el => el.click())
+        // await checkInfoMatches();
+        await driver.actions()
+            .scroll(0, 0, 0, 0, await getElementBy("id", "tuaFee", driver))
+            .perform();
+        await getElementBy("css", ".checkforprice", driver)
+            .then(el => el.click());
+        await getElementBy("css", "#ContinueToSeat button", driver)
+            .then(el => el.click());
+        await driver.sleep(7000);
+        await getElementBy("css", "mat-grid-tile.available")
+            .then(el => el.click());
+        const seatsButton = await getElementBy("css", ".pax-tab-container button", driver);
+        await seatsButton.click();
+        await driver.wait(until.elementTextMatches(seatsButton, "Continue to payment"), 5000);
+        await driver.sleep(7000);
+        await getElementBy("css", ".continue", driver)
+            .then(el => el.click())
+        await getElementBy("css", "input[formcontrolname = 'fName']")
+            .then(el => el.sendKeys("Alex"));
+        await getElementBy("css", "input[formcontrolname = 'lName']")
+            .then(el => el.sendKeys("Lora"));
+        await getElementBy("css", "input[formcontrolname = 'dobDay']")
+            .then(el => el.sendKeys("16"));
+        await getElementBy("css", "input[formcontrolname = 'dobMonth']")
+            .then(el => el.sendKeys("Jan"));
+        await getElementBy("css", "input[formcontrolname = 'dobYear']")
+            .then(el => el.sendKeys("1998"));
+        await getElementBy("css", ".nationality input")
+            .then(el => el.click());
+        await getElementBy("css", "mat-option", driver)
+            .then(el => el.click())
+        await getElementBy("xpath", "(//div[contains(@class, 'primary-customer-section')]/following-sibling::div)[1] //button", driver)
+            .then(el => el.click())
     } catch (err) {
         console.log(err);
     } finally {
--- utils/mapper.js
+++ utils/mapper.js
@@ -1,23 +1,21 @@
 
-const fromObjectToObjectArray = ({DT_URL, DT_DEPARTURE, DT_DESTINATION}) => {
+const fromObjectToObjectArray = ({DT_URL, ...data}) => {
     const mappedData = [];
     let hasUrl = false;
     const sortedDepartureDestination = Object
-        .entries({DT_DEPARTURE, DT_DESTINATION})
+        .entries(data)
         .sort((a, b) => b[1].length - a[1].length);
     for (let pairIndex = 0; pairIndex < sortedDepartureDestination.length; pairIndex++) {
-        let airportIndex = 0;
-        while (airportIndex < sortedDepartureDestination[pairIndex][1].length) {
+        
+        for (let airportIndex = 0; airportIndex < sortedDepartureDestination[pairIndex][1].length; airportIndex++) {
             if (mappedData.length >= sortedDepartureDestination[pairIndex][1].length && !mappedData[airportIndex].hasOwnProperty(sortedDepartureDestination[pairIndex][0])) {
                 mappedData[airportIndex] = {...mappedData[airportIndex], [sortedDepartureDestination[pairIndex][0]]: sortedDepartureDestination[pairIndex][1][airportIndex]};
             } else {
                 mappedData.push({DT_URL: hasUrl ? null : DT_URL, [sortedDepartureDestination[pairIndex][0]]: sortedDepartureDestination[pairIndex][1][airportIndex]});
+                hasUrl = true;
             }
-            hasUrl = true;
-            airportIndex++;
         }
     }
-    console.log(mappedData);
     return mappedData;
 }
 
